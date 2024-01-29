d4fe4d28c1db8dc48bf2b5d3eb42d828e0f68728
dali546 <35352237+dali546@users.noreply.github.com>
2024-01-29 12:11:13 +0000

    feat: setup terraform bug

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.7.0"
+  required_providers {
+    name = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+module "nlb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.4.1"
+}
